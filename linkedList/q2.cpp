ff678c064e64b295e86f4e4a0ae495ea4cb3a12c
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2025-12-30 17:51:05 +0530

    Linked List

---
+++ linkedList/q2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+//detection of a cycle in a linked list
+//time complexity : O(N)
+//space complexity : O(1)
+
+bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast) return true;
+        }
+
+        return false;
+    }
