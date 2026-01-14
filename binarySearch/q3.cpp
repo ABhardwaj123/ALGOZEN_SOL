645fb037ccf9b79e035dc5987474ab506d1e7c70
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-01-14 19:24:51 +0530

    search in rotated sorted array

---
+++ binarySearch/q3.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//searching in a rotated sorted array with duplicates
+//TC : O(logN)
+//SC: O(1)
+
+bool search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <= right){
+            int mid = (left + right)/2;
+
+            if(nums[mid] == target){
+                return true;
+            }
+
+            if(nums[mid] == nums[right] && nums[mid] == nums[left]){//we cant determine the sorted space
+                left++;
+                right--;
+                continue;
+            }
+
+            if(nums[mid] <= nums[right]){//right sorted
+                if(target > nums[mid] && target <= nums[right]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }else{//left sorted
+                if(target >= nums[left] && target < nums[mid]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }
+
+        }
+
+        return false;
+    }
